6b52a2c060fc01fbfeffc9345e93741061ece457
kunall77 <62241367+kunall77@users.noreply.github.com>
2023-10-13 07:30:15 +0000

    dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553;
 let accountEmail = "example@gmail.com";
 var accountPassword = "12345";
 accountCity = "New Delhi";
+let accountState;
 
 // accountId = 2; // not allowed
 
@@ -11,5 +12,15 @@ accountCity = "Bengaluru";
 
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table(
+    [accountId, accountEmail, accountPassword, accountCity, accountState]
+);
+
+
+    
 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); // we are using node js not browser
+// alert(3 + 3);
+
+console.log(3 + 3); 
+// code readability is high priority
+console.log("kunal");
+
+// JS original documentation:  https://tc39.es/ecma262/
+// and MDN web docs
+
+// Data Types:
+
+let name = "Kunal";
+let age = 18;
+let isLoggedIn = false;
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> aabhi value assign nhi hue hai
+// symbol -> unique
+
+// object
+
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
