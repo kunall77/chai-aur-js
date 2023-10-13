8e57a9cf15bd60144a69e40d89d5764bcf999daa
kunall77 <62241367+kunall77@users.noreply.github.com>
2023-10-13 07:53:27 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "true";
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1; false -> 0
+// null -> 0
+// undefined -> NaN
+
+let isLoggedIn = "kunal";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true; 0 -> false
+// "" -> false
+// "kunal" -> true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
