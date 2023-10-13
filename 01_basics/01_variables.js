c7e1e3315b7474c4ca8399cb57c99d20f7164fe5
kunall77 <62241367+kunall77@users.noreply.github.com>
2023-10-13 06:19:09 +0000

    variables,js coded

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "example@gmail.com";
+var accountPassword = "12345";
+accountCity = "New Delhi";
+
+// accountId = 2; // not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "212121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
