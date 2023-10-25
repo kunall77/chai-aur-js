ece7b68a4e5917e0414b0275907fef90519c803f
kunall77 <62241367+kunall77@users.noreply.github.com>
2023-10-25 10:01:27 +0000

    functions 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,34 @@ function logInUserMessage(username = "elon") {
 }
 
 // console.log(logInUserMessage("Kunal"));
-console.log(logInUserMessage());
+// console.log(logInUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "kunal",
+    prices: 199
+};
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price: 399
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+// console.log((returnSecondValue(myNewArray)));
+console.log((returnSecondValue([200, 400, 100, 600])));
