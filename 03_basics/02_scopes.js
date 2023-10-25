a63c36c4dbb76f5b33fa53cc4f1805115fb5f223
kunall77 <62241367+kunall77@users.noreply.github.com>
2023-10-25 11:42:20 +0000

    scopes 01

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+
+// scope => {}
+
+// var c = 300;
+
+let a = 300;
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    // var c = 30;
+    console.log("Inner:", a);
+}
+
+console.log("Global:", a);
+// console.log(b);
+// console.log(c);
+
